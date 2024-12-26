31994bb62a309a3860e25840c864f9d8b58eea0d
Batyr <bshekish@gmail.com>
2024-12-26 19:26:38 +0500

    Second commit

--- gauss_method.py
+++ gauss_method.py
@@ -1,25 +1,21 @@
 import numpy as np
 
-def gaussian_elimination_with_pivoting(A, b):
+def gaussian_elimination(A, b):
     n = len(b)
     
+    # Forward elimination
     for k in range(n-1):
         if A[k, k] == 0:
-            for i in range(k+1, n):
-                if A[i, k] != 0:  
-                    A[[k, i]] = A[[i, k]]  
-                    b[k], b[i] = b[i], b[k]  
-                    break
-            else:
-                print(f"Warning: Column {k} has no non-zero pivot element, so, skip.")
-                continue
-
+            print(f"Warning: Column {k} has zero pivot element.")
+            return None
+        
         for i in range(k+1, n):
             c = A[i, k] / A[k, k]
             for j in range(k, n):
                 A[i, j] -= c * A[k, j]
             b[i] -= c * b[k]
 
+    # Back substitution
     x = np.zeros(n)
     x[-1] = b[-1] / A[-1, -1]
     for i in range(n-2, -1, -1):
@@ -39,6 +35,6 @@ A = np.array([
 
 b = np.array([18, 26, 34, 82], dtype=float)
 
-solution = gaussian_elimination_with_pivoting(A, b)
+solution = gaussian_elimination(A, b)
 
 print("Solution:", solution)
